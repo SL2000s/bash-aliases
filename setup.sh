fa1d2e42d5ea6e61a6c0e1d12591fe95a1e715c4
Simon Ljungbeck <simon.ljungbeck@gmail.com>
2025-01-01 17:19:23 +0100

    style: rename bash_aliases to .bash_aliases

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
-touch ~/bash_aliases/system_aliases
+touch ~/.bash_aliases/system_aliases
 echo "Add these lines to .bashrc or .zshrc:
-if [ -f ~/bash_aliases/common_aliases ]; then\nOA
-    . ~/bash_aliases/common_aliases
+if [ -f ~/.bash_aliases/common_aliases ]; then
+    . ~/.bash_aliases/common_aliases
 fi
-if [ -f ~/bash_aliases/system_aliases ]; then
-    . ~/bash_aliases/system_aliases
+if [ -f ~/.bash_aliases/system_aliases ]; then
+    . ~/.bash_aliases/system_aliases
 fi"
