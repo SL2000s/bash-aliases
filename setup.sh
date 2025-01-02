739752d97032d93684953dc45760a2b66c6b46c8
Simon Ljungbeck <simon.ljungbeck@gmail.com>
2025-01-02 17:47:35 +0100

    feat: update setup script

--- setup.sh
+++ setup.sh
@@ -1,8 +1,16 @@
-touch ~/.bash_aliases/system_aliases
+#!/bin/bash
+
+SCRIPT_DIR=$(dirname "$(realpath "${BASH_SOURCE[0]}")")
+COMMON_BASH_ALIASES_PATH="$SCRIPT_DIR/common_aliases"
+SYSTEM_BASH_ALIASES_PATH="$SCRIPT_DIR/system_aliases"
+
+touch "$SYSTEM_BASH_ALIASES_PATH"
 echo "Add these lines to .bashrc or .zshrc:
-if [ -f ~/.bash_aliases/common_aliases ]; then
-    . ~/.bash_aliases/common_aliases
+COMMON_BASH_ALIASES_PATH=\"$COMMON_BASH_ALIASES_PATH\"
+SYSTEM_BASH_ALIASES_PATH=\"$SYSTEM_BASH_ALIASES_PATH\"
+if [ -f \"\$COMMON_BASH_ALIASES_PATH\" ]; then
+    . \"\$COMMON_BASH_ALIASES_PATH\"
 fi
-if [ -f ~/.bash_aliases/system_aliases ]; then
-    . ~/.bash_aliases/system_aliases
+if [ -f \"\$SYSTEM_BASH_ALIASES_PATH\" ]; then
+    . \"\$SYSTEM_BASH_ALIASES_PATH\"
 fi"
