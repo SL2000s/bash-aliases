b402a8221e9e9dd8452ffb8be342f774583e1ce7
Simon Ljungbeck <simon.ljungbeck@gmail.com>
2025-01-18 13:59:32 +0100

    feat: create a directory bashrc.d/ for the aliases

--- setup.sh
+++ setup.sh
@@ -3,14 +3,31 @@
 SCRIPT_DIR=$(dirname "$(realpath "${BASH_SOURCE[0]}")")
 COMMON_BASH_ALIASES_PATH="$SCRIPT_DIR/common_aliases"
 SYSTEM_BASH_ALIASES_PATH="$SCRIPT_DIR/system_aliases"
+BASHRC_D_DIR="$HOME/.bashrc.d"
 
-touch "$SYSTEM_BASH_ALIASES_PATH"
-echo "Add these lines to .bashrc or .zshrc:
-COMMON_BASH_ALIASES_PATH=\"$COMMON_BASH_ALIASES_PATH\"
-SYSTEM_BASH_ALIASES_PATH=\"$SYSTEM_BASH_ALIASES_PATH\"
-if [ -f \"\$COMMON_BASH_ALIASES_PATH\" ]; then
-    . \"\$COMMON_BASH_ALIASES_PATH\"
+# Ensure the .bashrc.d directory exists
+if [ ! -d "$BASHRC_D_DIR" ]; then
+    mkdir -p "$BASHRC_D_DIR"
+    echo "Created directory: $BASHRC_D_DIR"
 fi
-if [ -f \"\$SYSTEM_BASH_ALIASES_PATH\" ]; then
-    . \"\$SYSTEM_BASH_ALIASES_PATH\"
-fi"
+
+# Create symlinks for the aliases files in ~/.bashrc.d
+ln -sf "$COMMON_BASH_ALIASES_PATH" "$BASHRC_D_DIR/common_aliases"
+ln -sf "$SYSTEM_BASH_ALIASES_PATH" "$BASHRC_D_DIR/system_aliases"
+
+# Create system_aliases file if it doesn't exist
+if [ ! -f "$SYSTEM_BASH_ALIASES_PATH" ]; then
+    touch "$SYSTEM_BASH_ALIASES_PATH"
+    echo "Created file: $SYSTEM_BASH_ALIASES_PATH"
+fi
+
+# Print updated instructions
+echo "Add this to your .bashrc or .zshrc:
+if [ -d ~/.bashrc.d ]; then
+    for rc in ~/.bashrc.d/*; do
+        if [ -f \"\$rc\" ]; then
+            . \"\$rc\"
+        fi
+    done
+fi
+unset rc"
